c5d59a23705a7db31958f2ae50660fd540020ed5
Jesus-30 <64994313+Jesus-30@users.noreply.github.com>
2020-05-16 16:03:04 -0600

    Agregué la clase con mi nombre

---
+++ src/proyecto_fianal/Jesus_Henriquez.java
@@ -0,0 +1,10 @@
+/****************************************
+Nombre: Jesús Adalberto Henríquez Blanco
+Carrera: Sistemas Imformáticos 
+Aula: SIS 22 "B"
+******************************************/
+package proyecto_fianal;
+
+public class Jesus_Henriquez {
+    
+}
