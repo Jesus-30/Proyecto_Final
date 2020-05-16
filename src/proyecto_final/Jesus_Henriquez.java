6a2e745dd68dbba93771ab01f86cdda84f0d9e5c
Jesus-30 <64994313+Jesus-30@users.noreply.github.com>
2020-05-16 16:11:03 -0600

    Corrgí error de Ortografia

--- src/proyecto_final/Jesus_Henriquez.java
+++ src/proyecto_final/Jesus_Henriquez.java
@@ -3,7 +3,7 @@ Nombre: Jesús Adalberto Henríquez Blanco
 Carrera: Sistemas Imformáticos 
 Aula: SIS 22 "B"
 ******************************************/
-package proyecto_fianal;
+package proyecto_final;
 
 public class Jesus_Henriquez {
     
